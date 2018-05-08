930f709aa9739e1db2ce60ac107b92b9aecd5a33
Vinicius Silva <vinicius.silva@virtus.ufcg.edu.br>
2018-05-08 11:25:31 -0300

    criando telas para menu de configurações

--- myAppIonic/src/app/app.module.ts
+++ myAppIonic/src/app/app.module.ts
@@ -1,19 +1,22 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
 
 //import { AboutPage } from '../pages/about/about';
 //import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import {HomePage} from '../pages/home/home';
+import {TabsPage} from '../pages/tabs/tabs';
 import {FeedPageModule} from '../pages/feed/feed.module';
 import {IntroPageModule} from '../pages/intro/intro.module'
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { MovieProvider } from '../providers/movie/movie';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {MovieProvider} from '../providers/movie/movie';
 import {HttpClientModule} from "@angular/common/http";
+import {ConfigurationsPageModule} from "../pages/configurations/configurations.module";
+import {ProfilePageModule} from "../pages/profile/profile.module";
+import {AboutPageModule} from "../pages/sobre/about.module";
 
 @NgModule({
   declarations: [
@@ -28,14 +31,17 @@ import {HttpClientModule} from "@angular/common/http";
     IonicModule.forRoot(MyApp),
     FeedPageModule,
     IntroPageModule,
-    HttpClientModule
+    HttpClientModule,
+    ConfigurationsPageModule,
+    ProfilePageModule,
+    AboutPageModule
 
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-   // AboutPage,
-   // ContactPage,
+    // AboutPage,
+    // ContactPage,
     HomePage,
     TabsPage
   ],
