6c278765cdc9074f908ea1e2aaa7ba1ada6aa871
Vinicius Silva <vinicius.silva@virtus.ufcg.edu.br>
2018-05-08 10:16:11 -0300

    criando estrutura de armazenamento localStorage

---
+++ myAppIonic/src/providers/config/config.ts
@@ -0,0 +1,59 @@
+import {Injectable} from '@angular/core';
+
+/*
+  Generated class for the MovieProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+let config_key_name = "config";
+
+@Injectable()
+export class ConfigProvider {
+
+  private config = {
+    showSlide: false,
+    name: "",
+    userName: ""
+  }
+
+  constructor() {
+  }
+
+  //Recupera os dados do localStorage
+  public getConfigData(): any {
+    //if ternario
+    return localStorage.getItem(config_key_name) || {};
+  }
+
+  //Grava os dados do localStorage
+  // Parâmetro com ? = significa que é o parâmetro é opcional
+  public setConfigData(showSlide?: boolean, name?: string, userName?: string) {
+    let config = {
+      showSlide: false,
+      name: "",
+      userName: ""
+    }
+
+    //atualiza showSlide
+    if (showSlide) {
+      //grava showSlide
+      config.showSlide = showSlide;
+    }
+    //atualiza name
+    if (name) {
+      //grava name
+      config.name = name;
+    }
+    //atualiza userName
+    if (userName) {
+      //grava userName
+      config.userName = userName;
+    }
+
+    //Gravando o config
+    localStorage.setItem(config_key_name, JSON.stringify(config));
+
+  }
+
+}
