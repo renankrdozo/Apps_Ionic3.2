acab2f2a506c11426815aab0cf7d046ae73553a5
Vinicius Silva <vinicius.silva@virtus.ufcg.edu.br>
2018-05-10 10:48:01 -0300

    verificando se o usuario esta logado

--- nutritionistApp/src/app/app.component.ts
+++ nutritionistApp/src/app/app.component.ts
@@ -1,21 +1,42 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { HomePage } from '../pages/login/login';
+import {Component} from '@angular/core';
+import {Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {HomePage} from '../pages/login/login';
+import {AngularFireAuth} from "angularfire2/auth";
+import {DicasPage} from "../pages/dicas/dicas";
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+              fireAuth: AngularFireAuth) {
+
+    const authObserver = fireAuth.authState.subscribe(users => {
+      this.checkingLoggedInUser(users, authObserver)
+    })
+
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  //verificando se o usuário esta logado para não exibir a tela de login.
+  private checkingLoggedInUser(users: any, authObserver: any) {
+    if (users) {
+      this.rootPage = DicasPage;
+      authObserver.unsubscribe();
+    } else {
+      this.rootPage = HomePage;
+      authObserver.unsubscribe();
+    }
   }
 }
 
